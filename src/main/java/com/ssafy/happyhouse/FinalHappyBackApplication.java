bbccb44f50f1205929b72551463490a84fb3270d
at8in <at8in@naver.com>
2021-05-20 09:25:40 +0900

    start

---
+++ src/main/java/com/ssafy/happyhouse/FinalHappyBackApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.happyhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalHappyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalHappyBackApplication.class, args);
+	}
+
+}
