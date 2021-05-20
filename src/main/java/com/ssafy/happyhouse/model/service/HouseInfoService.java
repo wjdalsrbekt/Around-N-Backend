504d31d8adedfb2cb5fbbe379357e639d8997cc8
정민규 <wjdalsrbekt@gmail.com>
2021-05-20 17:27:09 +0900

    상세검색 및 전체검색 합쳐 구현

--- src/main/java/com/ssafy/happyhouse/model/service/HouseInfoService.java
+++ src/main/java/com/ssafy/happyhouse/model/service/HouseInfoService.java
@@ -7,12 +7,7 @@ import com.ssafy.happyhouse.model.HouseInfoDto;
 import com.ssafy.happyhouse.model.SearchDto;
 
 public interface HouseInfoService {
-
-	List<HouseInfoDto> list();
-
-	List<HouseInfoDto> apt_search(SearchDto searchDto);
-
-//	List<HouseInfoDto> dong_search(String dong);
+	List<HouseInfoDto> list(SearchDto searchDto);
 
 	boolean create(HouseInfoDto houseInfoDto);
 
