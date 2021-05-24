4a5880c004a46e720c272b4a7cae224494dcb377
정민규 <wjdalsrbekt@gmail.com>
2021-05-24 17:37:23 +0900

    0524 commit

--- src/main/java/com/ssafy/happyhouse/controller/BoardRestController.java
+++ src/main/java/com/ssafy/happyhouse/controller/BoardRestController.java
@@ -121,18 +121,59 @@ public class BoardRestController {
 	}
 
 	/////////////////////////
+//	@ApiOperation(value = "bnum에 해당하는 게시판의 정보를 수정한다. 그리고 DB수정 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+//	@PutMapping
+//	public ResponseEntity<String> modifyBoard(@RequestBody BoardDto boardDto) {
+//		logger.debug("modifyBoard - 호출");
+//		logger.debug("" + boardDto);
+//		System.out.println(boardDto);
+//		if (boardService.modify(boardDto)) {
+//			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+//		}
+//		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+//	}
+
 	@ApiOperation(value = "bnum에 해당하는 게시판의 정보를 수정한다. 그리고 DB수정 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
 	@PutMapping
-	public ResponseEntity<String> modifyBoard(@RequestBody BoardDto boardDto) {
+	public ResponseEntity<String> modifyBoard(BoardDto boardDto, @RequestParam(value="upfile", required = false) MultipartFile files) throws IllegalStateException, IOException {
 		logger.debug("modifyBoard - 호출");
-		logger.debug("" + boardDto);
 		System.out.println(boardDto);
-		if (boardService.modify(boardDto)) {
-			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		System.out.println(files.toString());
+		logger.debug("" + boardDto);
+		if (files != null && !files.isEmpty()) {
+//			String realPath = servletContext.getRealPath("/resources/img");
+			String realPath = servletContext.getRealPath("/upload");
+			String today = new SimpleDateFormat("yyMMdd").format(new Date());
+			String saveFolder = realPath + File.separator + today;
+			File folder = new File(saveFolder);
+			if (!folder.exists()) {
+				System.out.println("####");
+				folder.mkdirs();
+			}
+			List<FileInfoDto> fileInfos = new ArrayList<FileInfoDto>();
+			FileInfoDto fileInfoDto = new FileInfoDto();
+			String originalFileName = files.getOriginalFilename();
+			if (!originalFileName.isEmpty()) {
+				String saveFileName = UUID.randomUUID().toString()
+						+ originalFileName.substring(originalFileName.lastIndexOf('.'));
+				fileInfoDto.setSaveFolder(today);
+				fileInfoDto.setOriginFile(originalFileName);
+				fileInfoDto.setSaveFile(saveFileName);
+				files.transferTo(new File(folder, saveFileName));
+			}
+			fileInfos.add(fileInfoDto);
+			boardDto.setFileInfos(fileInfos);
 		}
-		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+		try {
+			boardService.modify(boardDto);
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+//			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		}
 	}
-
+	
 	@ApiOperation(value = "bnum에 해당하는 게시판의 정보를 삭제한다. 그리고 DB삭제 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
 	@DeleteMapping("{bnum}")
 	public ResponseEntity<String> deleteBoard(@PathVariable("bnum") int bnum) {
